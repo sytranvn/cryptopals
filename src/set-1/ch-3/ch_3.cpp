ca57e99c7e0f8824dc772ef98c48d76f71613cc1
Sy Tran <13009812+sytranvn@users.noreply.github.com>
2023-10-09 22:53:50 +0700

    fix gcc
    Signed-off-by: Sy Tran <13009812+sytranvn@users.noreply.github.com>

--- src/set-1/ch-3/ch_3.cpp
+++ src/set-1/ch-3/ch_3.cpp
@@ -13,9 +13,9 @@ float freq_table[95]{
     5.92, 7.31,  0.1,  0.69, 3.98, 2.61, 6.95, 7.68, 1.82, 0.11,  6.02, 6.28,
     9.1,  2.88,  1.11, 2.09, 0.17, 2.11, 0.07, 0,    0,    0,     0};
 
-int freq_of(const char *str, int len) {
+int freq_of(const char *str) {
   float s = 0;
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < strlen(str); i++) {
     s += freq_table[str[i] - 32];
   }
   return s;
@@ -29,7 +29,7 @@ void single_byte_xor_cipher(const char *str, char **out, char *cipher) {
   char *cur = (char *)malloc(sizeof(char) * (len + 1));
   *out = (char *)malloc(sizeof(char) * (len + 1));
   float max_freq = 0;
-  float req_cur = 0;
+  float freq_cur = 0;
 
   // try xor each printable character with current string
   for (std::uint8_t i = 0; i < 95; i++) {
@@ -37,15 +37,15 @@ void single_byte_xor_cipher(const char *str, char **out, char *cipher) {
     std::uint8_t ch = i + 32;
 
     for (int j = 0; j < len; j++) {
-      std::uint8_t r = ch ^ buff[j];
+      std::uint8_t r = (std::uint8_t)ch ^ (std::uint8_t)buff[j];
       if (r < 32 || r >= 127) {
         break;
       }
       cur[j] = r;
     }
-    req_cur = freq_of(cur, len);
-    if (req_cur > max_freq) {
-      max_freq = req_cur;
+    freq_cur = freq_of(cur);
+    if (freq_cur > max_freq) {
+      max_freq = freq_cur;
       strcpy(*out, cur);
       *cipher = ch;
     }
