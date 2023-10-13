2561baaa5ebed526302a4a76c263d2055dad5e08
Sy Tran Dung <13009812+sytranvn@users.noreply.github.com>
2023-10-13 11:28:12 +0700

    setup ch5

---
+++ src/set-1/ch-5/ch_5.cpp
@@ -0,0 +1,3 @@
+#include "ch_5.h"
+
+void repeating_key_xor(const char *str, const char *key, char **out);
---
+++ src/set-1/ch-5/ch_5.h
@@ -0,0 +1,6 @@
+#ifndef CH_5_H_
+#define CH_5_H_
+
+void repeating_key_xor(const char *str, const char *key, char **out);
+
+#endif
